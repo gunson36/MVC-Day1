c3e78cda05f09453f4176e8cacaa843be4f6d5dd
anson <anson@X230>
2016-04-14 23:08:00 +0800

    加入專案檔案。

---
+++ Day1/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Day1.Models;
+
+namespace Day1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        //public ActionResult MoneyList()
+        //{
+        //    var lstMoney = new List<Money>();
+        //    for (int i = 0; i < 5; i++) { 
+        //        lstMoney.Add(new Money
+        //        {
+        //            seqno = 1 + i,
+        //            category = "支出",
+        //            amount = 100 * i,
+        //            date = DateTime.Now.AddDays(1 * i),
+        //            remark = "鉛筆"
+        //        });
+        //    }
+        //    return View(lstMoney);
+        //}
+        public ActionResult MoneyList()
+        {
+            var modelView = new List<Money>();
+            for (int i = 0; i < 5; i++)
+            {
+                modelView.Add(new Money
+                {
+                    seqno = 1 + i,
+                    category = "支出",
+                    amount = 100 * i + 2,
+                    date = DateTime.Now.AddDays(1 * i),
+                    remark = "鉛筆"
+                });
+            }
+            return View(modelView);
+        }
+
+    }
+}
---
+++ Day1/Models/MoneyModels.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Day1.Models
+{
+    public class Money
+    {
+        [DisplayName("序號")]
+        public int seqno { get; set; }
+
+        [DisplayName("類別")]
+        public string category { get; set; }
+        [DisplayName("日期")]
+        public DateTime date { get; set; }
+        [DisplayName("金額")]
+        public decimal amount { get; set; }
+        [DisplayName("備註")]
+        public string remark { get; set; }
+    }
+
+    public class MoneyViewModel
+    {
+        public IEnumerable<Money> MoneyList { get; set; }    
+    }
+}
